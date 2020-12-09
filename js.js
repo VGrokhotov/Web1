91bd8bac11fa0fb1f9e3cd30acf84fa8e9bda512
Vladislav <vlad@grokhotov.ru>
2020-12-09 19:12:53 +0300

    lab 6 finished

--- js.js
+++ js.js
@@ -1,100 +1,152 @@
-var arr = [];
+///////////////////////////////////////////////////LAB 5
 
-for (i = 0; i < 1001; i++) {
-  var a = Math.round(Math.random() * 1000);
-  arr.push(a);
+// var arr = [];
+
+// for (i = 0; i < 1001; i++) {
+//   var a = Math.round(Math.random() * 1000);
+//   arr.push(a);
+// }
+
+// function min(array) {
+//   var min = 100000000000000;
+//   for (number of arr) {
+//     if (min > number) {
+//       min = number;
+//     }
+//   }
+//   return min;
+// }
+
+// function max(array) {
+//   var max = -100000000000000;
+//   for (number of arr) {
+//     if (number > max) {
+//       max = number;
+//     }
+//   }
+//   return max;
+// }
+
+// function compareNumeric(a, b) {
+//   if (a > b) return 1;
+//   if (a == b) return 0;
+//   if (a < b) return -1;
+// }
+
+// function median(array) {
+//   var newArr = arr.slice().sort(compareNumeric);
+//   return newArr[Math.floor(newArr.length / 2)];
+// }
+
+// var minimun = min(arr);
+// var maximum = max(arr);
+// var mediana = median(arr);
+
+// console.log(minimun, maximum, mediana);
+
+// function swap(array, firstIndex, secondIndex) {
+//   const temp = array[firstIndex];
+//   array[firstIndex] = array[secondIndex];
+//   array[secondIndex] = temp;
+// }
+
+// function partition(array, left, right) {
+//   var pivot = array[Math.floor((right + left) / 2)],
+//     i = left,
+//     j = right;
+//   while (i <= j) {
+//     while (array[i] < pivot) {
+//       i++;
+//     }
+//     while (array[j] > pivot) {
+//       j--;
+//     }
+//     if (i <= j) {
+//       swap(array, i, j);
+//       i++;
+//       j--;
+//     }
+//   }
+//   return i;
+// }
+
+// function quickSort(array, left, right) {
+//   var index;
+//   if (array.length > 1) {
+//     index = partition(array, left, right);
+//     if (left < index - 1) {
+//       quickSort(array, left, index - 1);
+//     }
+//     if (index < right) {
+//       quickSort(array, index, right);
+//     }
+//   }
+//   return array;
+// }
+
+// arr = quickSort(arr, 0, arr.length - 1);
+// console.log(arr);
+
+// var dict = new Map();
+// var info = document.querySelectorAll("*")
+// for (tag of info) {
+//   var tagName = tag.tagName
+//   if (dict.has(tagName)) {
+//     dict.set(tagName, dict.get(tagName) + 1 )
+//   } else {
+//     dict.set(tagName, 1)
+//   }
+// }
+// for (var [key, value] of dict) {
+//   console.log(key + ": " + value);
+// }
+
+
+///////////////////////////////////////////////////LAB 5 END
+
+// var a = document.querySelectorAll('.contactsButton');  
+// //перебираем все найденные элементы и вешаем на них события
+// [].forEach.call( a, function(element) {
+//     //вешаем событие
+//     element.onclick = function() {
+//       alert('Клик!');
+//     }
+// });
+
+
+var shadow = document.createElement('div');
+shadow.className = 'shadow';
+
+var contacts = document.querySelector('.contacts');
+contacts.className = 'contacts';
+contacts.style.display = 'flex';
+
+contacts.onclick = function(event) {
+  event.stopPropagation();
 }
 
-function min(array) {
-  var min = 100000000000000;
-  for (number of arr) {
-    if (min > number) {
-      min = number;
-    }
-  }
-  return min;
+shadow.onclick = function () {
+  shadow.removeChild(contacts);
+  document.body.removeChild(shadow);
+  contactsDiv.style.display = 'none';
 }
 
-function max(array) {
-  var max = -100000000000000;
-  for (number of arr) {
-    if (number > max) {
-      max = number;
-    }
-  }
-  return max;
+document.querySelector('.contactsButton').onclick = function () {
+  shadow.appendChild(contacts);
+  document.body.appendChild(shadow);
 }
 
-function compareNumeric(a, b) {
-  if (a > b) return 1;
-  if (a == b) return 0;
-  if (a < b) return -1;
-}
+var img = document.querySelector('img');
+images = ['vg1.jpg', 'vg2.jpeg', 'vg3.jpg'];
 
-function median(array) {
-  var newArr = arr.slice().sort(compareNumeric);
-  return newArr[Math.floor(newArr.length / 2)];
-}
+var currentIndex = 0; 
 
-var minimun = min(arr);
-var maximum = max(arr);
-var mediana = median(arr);
-
-console.log(minimun, maximum, mediana);
-
-function swap(array, firstIndex, secondIndex) {
-  const temp = array[firstIndex];
-  array[firstIndex] = array[secondIndex];
-  array[secondIndex] = temp;
-}
-
-function partition(array, left, right) {
-  var pivot = array[Math.floor((right + left) / 2)],
-    i = left,
-    j = right;
-  while (i <= j) {
-    while (array[i] < pivot) {
-      i++;
-    }
-    while (array[j] > pivot) {
-      j--;
-    }
-    if (i <= j) {
-      swap(array, i, j);
-      i++;
-      j--;
-    }
-  }
-  return i;
-}
-
-function quickSort(array, left, right) {
-  var index;
-  if (array.length > 1) {
-    index = partition(array, left, right);
-    if (left < index - 1) {
-      quickSort(array, left, index - 1);
-    }
-    if (index < right) {
-      quickSort(array, index, right);
-    }
-  }
-  return array;
-}
-
-arr = quickSort(arr, 0, arr.length - 1);
-console.log(arr);
-
-var dict = new Map();
-var info = document.querySelectorAll("*")
-for (tag of info) {
-  var tagName = tag.tagName
-  if (dict.has(tagName)) {
-    dict.set(tagName, dict.get(tagName) + 1 )
+img.onclick = function() {
+  console.log('lol');
+  if (currentIndex == images.length - 1) {
+    currentIndex = 0;
   } else {
-    dict.set(tagName, 1)
+    currentIndex++;
   }
-}
-for (var [key, value] of dict) {
-  console.log(key + ": " + value);
+  img.src = "img/" + images[currentIndex]
 }
